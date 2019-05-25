059dfda6df39369cc7409c7f2f0a8831bffed242
OlgaKazantseva <38510728+OlgaKazantseva@users.noreply.github.com>
2019-05-25 18:55:49 +0300

    Create 1ex.sql

---
+++ 1ex.sql
@@ -0,0 +1,10 @@
+--1.1
+
+INSERT INTO `theatre`.`Staff` (`idStaff`, `Surname`, `Name`, `Middlename`, `Passport`, `Position`, `Adress`, `Home_phone`, `Mobile_phone`) VALUES ('1', 'Казанцева', 'Ольга', 'Андреевна', '4167876', 'Директор', 'бабаба', '09896788', '89192736678');
+INSERT INTO `theatre`.`Staff` (`idStaff`, `Surname`, `Name`, `Middlename`, `Passport`, `Position`, `Adress`, `Home_phone`, `Mobile_phone`) VALUES ('2', 'Бибаев', 'Георгий', 'Александрович', '6757878', 'Актер', 'бубубу', '78909739', '89913746663');
+INSERT INTO `theatre`.`Staff` (`idStaff`, `Surname`, `Name`, `Middlename`, `Passport`, `Position`, `Adress`, `Home_phone`, `Mobile_phone`) VALUES ('3', 'Князева', 'Дарья', 'Андреевна', '7567898', 'Актер', 'бфбфбф', '89796987', '89716276257');
+INSERT INTO `theatre`.`Staff` (`idStaff`, `Surname`, `Name`, `Middlename`, `Passport`, `Position`, `Adress`, `Home_phone`, `Mobile_phone`) VALUES ('4', 'Васильченко', 'Антон', 'Владимирович', '8698745', 'Администратор', 'быбыбы', '94536678', '89483767287');
+INSERT INTO `theatre`.`Staff` (`idStaff`, `Surname`, `Name`, `Middlename`, `Passport`, `Position`, `Adress`, `Home_phone`, `Mobile_phone`) VALUES ('5', 'Бобиков', 'Станислав', 'Сергеевич', '0642313', 'Свет', 'блблбл', '09182338', '89346216567');
+INSERT INTO `theatre`.`Staff` (`idStaff`, `Surname`, `Name`, `Middlename`, `Passport`, `Position`, `Adress`, `Home_phone`, `Mobile_phone`) VALUES ('6', 'Гвоздарева', 'Ирина', 'Андреевна', '8977647', 'Актер', 'бобобо', '78685689', '89564563563');
+INSERT INTO `theatre`.`Staff` (`idStaff`, `Surname`, `Name`, `Middlename`, `Passport`, `Position`, `Adress`, `Home_phone`, `Mobile_phone`) VALUES ('7', 'Петров', 'Александр', 'Угович', '8757568', 'Актер', 'бебебе', '98674545', '89456452711');
+INSERT INTO `theatre`.`Staff` (`idStaff`, `Surname`, `Name`, `Middlename`, `Passport`, `Position`, `Adress`, `Home_phone`, `Mobile_phone`) VALUES ('8', 'Аджи-Асан', 'Аким', 'Диляверович', '8665765', 'Постановщик', 'брбрбр', '56464756', '89554354567');

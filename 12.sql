3231406c7c0c8810d1c3187b8eacebf9daa727a1
OlgaKazantseva <38510728+OlgaKazantseva@users.noreply.github.com>
2019-05-27 21:06:58 +0300

    Update 12.sql

--- 12.sql
+++ 12.sql
@@ -4,3 +4,15 @@
 UPDATE S_S
 set `Staff_idStaff`=3
 where  (role like "%Свет%" and S_S.idS_S=3);
+--12.2
+/*изменить стоимость самого дешевого спектакля*/
+update `Schedules`
+set Cost = 1000
+where(`idSchedule` in( select t1.`idSchedule` from
+(select sc.`idSchedule`,sc.Start,sc.Cost
+from Perfomances as p
+right join `Schedules` as sc
+on sc.`Perfomances_idPerfomance`=p.`idPerfomance`
+order by sc.Cost desc
+limit 1) as t1));
+                      --12.3
